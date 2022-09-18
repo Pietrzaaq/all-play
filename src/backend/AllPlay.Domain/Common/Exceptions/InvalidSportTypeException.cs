bba9c26a3297d2918176d754b769afbe6343ec46
Pietrzaaq <pietrzakd53@gmail.com>
2022-09-19 00:28:18 +0200

    Added first map business classes

---
+++ src/backend/AllPlay.Domain/Common/Exceptions/InvalidSportTypeException.cs
@@ -0,0 +1,8 @@
+﻿namespace AllPlay.Domain.Common.Exceptions;
+public sealed class InvalidSportTypeException : AllPlayException
+{
+    public InvalidSportTypeException() : 
+        base("Defined sport type is invalid")
+    {
+    }
+}
---
+++ src/backend/AllPlay.Domain/Repositories/IPinRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AllPlay.Domain.Repositories;
+
+public interface IPinRepository
+{
+    Task<Pin> GetAsync(int pinId);
+    Task<bool> ExistsAsync(Coordinates coorditanes);
+    Task<IReadOnlyList<Pin>> BrowseAsync();
+}
