9bea530701da6593cd53cadf4ce92af5cb3a1ef8
Pietrzaaq <pietrzakd53@gmail.com>
2022-11-10 20:07:08 +0100

    Changed project folder structure

--- src/backend/AllPlay.Domain/Common/Exceptions/InvalidSportTypeException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AllPlay.Domain.Common.Exceptions;
-public sealed class InvalidSportTypeException : AllPlayException
-{
-    public InvalidSportTypeException() : 
-        base("Defined sport type is invalid")
-    {
-    }
-}
