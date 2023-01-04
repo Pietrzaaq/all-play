e905b4c79a89044141d541811a0136857581ce59
Pietrzaaq <pietrzakd53@gmail.com>
2023-01-05 00:45:45 +0100

    Added PhoneNumber value object

---
+++ src/AllPlay.Domain/Common/ValueObjects/PhoneNumber.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+using AllPlay.Domain.Exceptions;
+
+namespace AllPlay.Domain.Common.ValueObjects;
+
+public class PhoneNumber
+{
+    public string Value { get; }
+    private readonly string _phoneNumberRegex = @"^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$";
+
+    public PhoneNumber(String phoneNumber)
+    {
+        var regex = new Regex(_phoneNumberRegex);
+        
+        if (!regex.IsMatch(phoneNumber))
+        {
+            throw new InvalidPhoneNumberException(phoneNumber);
+        }
+
+        Value = phoneNumber;
+    }
+}
---
+++ src/AllPlay.Domain/Exceptions/InvalidPhoneNumberException.cs
@@ -0,0 +1,12 @@
+﻿namespace AllPlay.Domain.Exceptions;
+
+public class InvalidPhoneNumberException : AllPlayException
+{
+    public string PhoneNumber { get; }
+
+    public InvalidPhoneNumberException(string phoneNumber)
+        : base($"Phone number {phoneNumber} is invalid")
+    {
+        PhoneNumber = phoneNumber;
+    }
+}
